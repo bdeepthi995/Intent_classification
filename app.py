3f73483189d103e48e6ff6c3a39e56e718b037e4
Bedurukota Mani Deepika <122142634+bdeepthi995@users.noreply.github.com>
2024-06-17 20:22:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from sklearn.naive_bayes import MultinomialNB
 
 app = Flask(__name__)
 
-dataset = pd.read_csv('temp_dataset.csv')  
+# Load the dataset
+dataset = pd.read_csv('temp_dataset.csv')  # Change 'intent.csv' to the name of your CSV file
 X_train = dataset['Text']
 y_train = dataset['Intent']
 
+# Create a CountVectorizer to convert text data to numerical form
 vectorizer = CountVectorizer()
 X_train_vectorized = vectorizer.fit_transform(X_train)
 
+# Train the model
 model = MultinomialNB()
 model.fit(X_train_vectorized, y_train)
 
@@ -22,21 +25,30 @@ def home():
 @app.route('/classify', methods=['POST'])
 def classify_intent():
     try:
+        # Check if a file was uploaded
         if 'csv_file' not in request.files:
             return "No file uploaded"
 
+        # Read the uploaded CSV file
         csv_file = request.files['csv_file']
         if csv_file.filename == '':
             return "Empty file name"
 
         df = pd.read_csv(csv_file)
+
+        # Vectorize the input text
         input_text_vectorized = vectorizer.transform(df['Text'])
+
+        # Predict the intents
         predicted_intents = model.predict(input_text_vectorized)
 
+        # Add predicted intents to the DataFrame
         df['Predicted Intent'] = predicted_intents
 
+        # Convert DataFrame to CSV
         output_csv = df.to_csv(index=False)
 
+        # Create response with CSV data
         response = make_response(output_csv)
         response.headers['Content-Disposition'] = 'attachment; filename=result.csv'
         response.headers['Content-type'] = 'text/csv'
